ffca29351d3b95673aa16dc0c00299d92716f496
coralam17@gmail.com <coralam17@gmail.com>
2015-08-01 20:28:27 -0400

    Added image upload ability using filepicker

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,17 +1,59 @@
 Template.postSubmit.events({
+
+    'click #upload': function (e) {
+        e.preventDefault();
+        imageUrl = "https://www.filepicker.io/api/file/ds2EMKyOTLu0rmEwISNT";
+        filepicker.pick(
+            {
+                mimetypes: ['image/gif','image/jpeg','image/png'],
+                multiple: false
+            },
+            function(InkBlob){
+                imageUrl = InkBlob.url;
+                //var image = Images.findOne({userId:Meteor.userId()});
+                //if(image){
+                //    Images.update({_id:image._id},
+                //        {
+                //            $set:{
+                //                filepickerId:_.last(InkBlob.url.split("/"))
+                //            }
+                //        });
+                //}else{
+                //    Images.insert({
+                //        userId:Meteor.userId(),
+                //        filepickerId:_.last(InkBlob.url.split("/")),
+                //        createdAt:new Date()
+                //    });
+                //}
+            },
+            function(FPError){
+                //if(FPError && FPError.code !== 101)
+                //    alert(FPError.toString());
+            }
+        );
+    },
+
     'submit form': function(e) {
         e.preventDefault();
 
+        //if(!imageUrl){
+        //    return alert("please upload an image");
+        //} else {
+        //    console.log(imageUrl);
+        //}
+
         var post = {
-            url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val(),
-            time: $(e.target).find('[name=postTime]').val()
+            url: "https://www.filepicker.io/api/file/ds2EMKyOTLu0rmEwISNT",
+            title: $(e.target).find('[name=title]').val()
+            //time: $(e.target).find('[name=postTime]').val()
         };
 
         Meteor.call('postInsert', post, function(error, result) {
             // display the error to the user and abort
-            if (error)
+            if (error){
+                console.log("this fucking sucks");
                 return alert(error.reason);
+            }
 
             // show this result but route anyway
             if (result.postExists)
@@ -20,6 +62,7 @@ Template.postSubmit.events({
             Router.go('postPage', {_id: result._id});
         });
     }
+
 });
 
 /*
@@ -35,4 +78,7 @@ meaning form is waiting for method to succeed before redirecting
 - to demonstrate the Meteor method client parrallely simulating while happening on the server,
     take out Router.go blablbah, and then put in Router.go('postsList') outside the callback method
     and += " (server)" or client to the post.js
+
+
+
 */
